f307e95ed200787d2fcab1fa7c4b1f2bd8af93ee
yunsc <chan9243@gmail.com>
2018-11-22 02:26:03 +0900

    create QnA Page, add Index Page Message

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import firebase_admin
 from flask import Flask, render_template, request, session, flash
 from firebase_admin import credentials
 from firebase_admin import db, auth
-
+import time
 
 app = Flask(__name__)
 root_user = ""
@@ -14,6 +14,14 @@ try:
     })
 except ValueError:
     print("valueError")
+
+
+def fetch(login_id):
+    global root_user
+    root_user = login_id
+    print("@@@@@@@global@@@@@@@@@@")
+    print(root_user)
+    print("@@@@@@@global@@@@@@@@@@")
 
 
 @app.route('/')
@@ -28,6 +36,11 @@ def about():
 
 @app.route('/index', methods=['POST','GET'])
 def index():
+    now = time.localtime()
+    s = "%04d-%02d-%02d"%(now.tm_year, now.tm_mon, now.tm_mday)
+    month = "%02d"%(now.tm_mon)
+    print(s)
+
     try:
         login_id = request.form['login']
         login_pwd = request.form['password']
@@ -37,11 +50,7 @@ def index():
         print(login_pwd)
         print("#####")
 
-        global root_user
-        root_user = login_id
-        print("@@@@@@@global@@@@@@@@@@")
-        print(root_user)
-        print("@@@@@@@global@@@@@@@@@@")
+        fetch(login_id)
 
         current_user = firebase_admin.auth.get_user_by_email(login_id)
         print(current_user.uid)
@@ -53,27 +62,17 @@ def index():
         outdoor_hum = ref.child('outdoor_fan_hum').get()
         outdoor_temp = ref.child('outdoor_fan_temp').get()
 
-
         return render_template('index.html', current_user=current_user.uid,
                                indoor_hum=indoor_hum, indoor_temp=indoor_temp,
-                               outdoor_hum=outdoor_hum, outdoor_temp=outdoor_temp)
+                               outdoor_hum=outdoor_hum, outdoor_temp=outdoor_temp,
+                               time = s, month = month)
     except ValueError:
         print('valueError')
         return render_template('login_2.html')
     except KeyError:
         print('keyerror')
 
-        global root_user
-        login_id = root_user
-
-        print("#####")
-        print(login_id)
-        print("#####")
-
-        root_user = login_id
-        print("@@@@@@@global@@@@@@@@@@")
-        print(root_user)
-        print("@@@@@@@global@@@@@@@@@@")
+        fetch(login_id)
 
         current_user = firebase_admin.auth.get_user_by_email(login_id)
         print(current_user.uid)
@@ -84,7 +83,6 @@ def index():
         indoor_temp = ref.child('indoor_temp').get()
         outdoor_hum = ref.child('outdoor_fan_hum').get()
         outdoor_temp = ref.child('outdoor_fan_temp').get()
-
 
         return render_template('index.html', current_user=current_user.uid,
                                indoor_hum=indoor_hum, indoor_temp=indoor_temp,
@@ -141,6 +139,21 @@ def test():
 @app.route('/coming')
 def coming():
     return render_template('coming-soon.html')
+
+
+@app.route('/samsung')
+def samsung():
+    return render_template('samsung.html')
+
+
+@app.route('/question')
+def question():
+    return render_template('question.html')
+
+
+@app.route('/lg')
+def lg():
+    return render_template('lg.html')
 
 
 if __name__ == '__main__':
